2a7b5c9f2b2d841e6f48a3302bb26099534f9d9a
Tylone <1147756095@qq.com>
2022-06-25 15:38:35 +0800

    upload

--- Functional.h
+++ Functional.h
@@ -3,10 +3,17 @@
 
 namespace TinySTL{
 	//********** [unary_function] ****************
-	template <class _Arg, class _Result>
+	template <class Arg, class Result>
 	struct unary_function {
-		typedef _Arg argument_type;
-		typedef _Result result_type;
+		typedef Arg argument_type;
+		typedef Result result_type;
+	};
+	//********** [binary_function] ****************
+	template <class Arg1, class Arg2, class Result>
+	struct binary_function {
+		typedef Arg1 first_argument_type;
+		typedef Arg2 second_argument_type;
+		typedef Result result_type;
 	};
 	//********** [less] ****************
 	template<class T>
@@ -34,6 +41,20 @@ namespace TinySTL{
 	template<class T>
 	struct identity :public unary_function<T, T> {
 		const T& operator()(const T& x) const { return x; }
+	};
+	//********** [select1st] ****************
+	template <class Pair>
+	struct select1st : public unary_function<Pair, typename Pair::first_type> {
+		const typename Pair::first_type& operator()(const Pair& x) const {
+			return x.first;
+		}
+	};
+	//********** [select2nd] ****************
+	template <class Pair>
+	struct select2nd : public unary_function<Pair, typename Pair::second_type> {
+		const typename Pair::second_type& operator()(const Pair& x) const {
+			return x.second;
+		}
 	};
 }
 #endif
